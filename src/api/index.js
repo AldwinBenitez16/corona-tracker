0bdda0e80463a338b22a950c09161980f1166451
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2020-06-14 22:46:39 -0400

    Adds country picker

--- src/api/index.js
+++ src/api/index.js
@@ -2,11 +2,15 @@ import axios from "axios";
 
 const url = "https://covid19.mathdro.id/api";
 
-export const fetchData = async () => {
+export const fetchData = async (country = null) => {
+  let dynamicUrl = url;
+  if (country) {
+    dynamicUrl = `${url}/countries/${country}`;
+  }
   try {
     const {
       data: { confirmed, recovered, deaths, lastUpdate },
-    } = await axios.get(url);
+    } = await axios.get(dynamicUrl);
     const modifiedData = { confirmed, recovered, deaths, lastUpdate };
 
     return modifiedData;
@@ -25,6 +29,18 @@ export const fetchDailyData = async () => {
     }));
 
     return modifiedData;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const fetchCountries = async () => {
+  try {
+    const {
+      data: { countries },
+    } = await axios.get(`${url}/countries`);
+
+    return countries.map((country) => country.name);
   } catch (err) {
     console.log(err);
   }
